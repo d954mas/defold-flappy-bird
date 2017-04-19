76e7918380063ffe520cd9405fdedb1795be7aee
d954mas <d954mas@mail.ru>
2017-04-20 00:38:37 +0300

    some bug ?

--- lib/draw_color/screen.fp
+++
@@ -1,9 +0,0 @@
-varying mediump vec4 position;
-varying mediump vec2 var_texcoord0;
-
-uniform mediump vec4 color;
-uniform lowp sampler2D TEX0;
-
-void main(){
-     gl_FragColor = color;
-}
--- lib/screen_helper.lua
+++ lib/screen_helper.lua
@@ -2,15 +2,20 @@ local M = {}
 M.game_width = 136
 M.game_height = nil
 M.game_delta_x = 0
-M.dirt_pos_y = 0
 M.height_delta = 0
 M.game_projection = nil
 
 function M.calculate(screen_width,screen_height)
-	M.game_height = M.game_width * screen_height/screen_width
-	M.dirt_pos_y = 38 * screen_height/M.game_height * M.game_height/204 * 1.6
+	local height_to_width = screen_height/screen_width
+	M.game_height = M.game_width * height_to_width
 	M.height_delta = M.game_height - 204
-	M.game_projection =vmath.matrix4_orthographic(0, M.game_width , -M.height_delta, M.game_height +M.height_delta, -1, 1)
+	if(M.height_delta>=0)then
+		M.game_projection = vmath.matrix4_orthographic(0, M.game_width , -M.height_delta, M.game_height +M.height_delta, -1, 1)
+	else
+		local delta = (1.5 - height_to_width) * M.game_width
+		M.game_height = 204
+		M.game_projection = vmath.matrix4_orthographic(-delta, M.game_width + delta,0, M.game_height, -1, 1)
+	end	
 end	
 
 
