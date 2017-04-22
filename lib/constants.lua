9ad1dd2949a1ba3acc97aa14fa6b389d8e170ad0
d954mas <d954mas@mail.ru>
2017-04-22 20:34:20 +0300

    refactoring

--- lib/constants.lua
+++ lib/constants.lua
@@ -1,9 +1,27 @@
 local M = {}
 
+--game zone sizes. Game zone width is same in different aspect ration.Game zone height is scaled.
+--all people will always see game zone. But some people can see more dirt and sky.
+M.game_zone_width = 136
+M.game_zone_height = 204
+
+
+M.bird_position = vmath.vector3(33,M.game_zone_height/2,0.3)
+
+--all info about pipes
+M.pipes_speed = 10 -- pixels/s speed of pipes and grass
+M.pipe_top_size = vmath.vector3(24,14,0)
+M.pipe_start_pos = vmath.vector3(100,47,0.5)
+M.pipes_gap = 50
+M.pipe_width = 24
+M.pipe_gap = 49 + M.pipe_width/2
+M.pipe_top_gap = 45
+M.pipe_bot_height = 3
+M.pipe_top_height = 14
+
+
 M.gui_width = 640
 M.gui_height = 960
-M.sky_color = vmath.vector4(55/255, 80/255, 100/255, 1) -- sky and clear color
-M.grass_color = vmath.vector4(111/255, 186 / 255, 45 / 255, 1)
-M.dirt_color = vmath.vector4(147/255, 80/255, 27/255, 1)
+M.sky_color = vmath.vector4(55/255, 80/255, 100/255, 1) -- used as clear color in render
 
 return M
--- lib/screen_helper.lua
+++ lib/screen_helper.lua
@@ -1,5 +1,7 @@
+local constants = require("lib.constants")
+
 local M = {}
-M.game_width = 136
+M.game_width = constants.game_zone_width
 M.game_height = nil
 M.game_delta_x = 0
 M.height_delta = 0
@@ -8,12 +10,14 @@ M.game_projection = nil
 function M.calculate(screen_width,screen_height)
 	local height_to_width = screen_height/screen_width
 	M.game_height = M.game_width * height_to_width
-	M.height_delta = M.game_height - 204
+	M.height_delta = (M.game_height - constants.game_zone_height)/2
 	if(M.height_delta>=0)then
-		M.game_projection = vmath.matrix4_orthographic(0, M.game_width , -M.height_delta, M.game_height +M.height_delta, -1, 1)
+		--show more then game_zone_height
+		M.game_projection = vmath.matrix4_orthographic(0, M.game_width , -M.height_delta, M.game_height - M.height_delta, -1, 1)
 	else
+		--show game zone. But add offset in x.
 		local delta = (1.5 - height_to_width) * M.game_width
-		M.game_height = 204
+		M.game_height = constants.game_zone_height
 		M.game_projection = vmath.matrix4_orthographic(-delta, M.game_width + delta,0, M.game_height, -1, 1)
 	end	
 end	
