20b20e1ce4f62f0b521f643669a3f4ef9ae67c81
d954mas <d954mas@mail.ru>
2017-07-11 14:34:24 +0300

    refactor screen helper

---
+++ lib/hashes.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+M.INPUT_TOUCH=hash("touch")
+
+M.PHYSICS_MESSAGE_CONTACT   = hash("contact_point_response")
+M.PHYSICS_MESSAGE_COLLISION = hash("collision_response")
+M.PHYSICS_MESSAGE_TRIGGER = hash("trigger_response")
+
+
+return M
--- lib/screen_helper.lua
+++ lib/screen_helper.lua
@@ -1,31 +1,33 @@
 local constants = require("lib.constants")
 
-local minimum_height_to_width = 4/3
+local minimum_aspect = 4/3
 
 local M = {}
-M.game_width = constants.game_zone_width
-M.game_height = nil
-M.game_delta_x = 0
-M.height_delta = 0
 M.game_projection = nil
+M.max_height = 0
 
 --if screen ration is bigger then 3/2 show more dirt and sky
 --if ration in 4/3 to 3/2 zoom to center.Player will see all game zone in width 
---and part of game zone in height
+--and part of game zone in height. 
+
 --if ratio <4/3 add offset at x
 function M.calculate(screen_width,screen_height)
-	local height_to_width = screen_height/screen_width
-	M.game_height = M.game_width * height_to_width
-	M.height_delta = (M.game_height - constants.game_zone_height)/2
-	if(height_to_width>=minimum_height_to_width)then
-		--show more then game_zone_height
-		M.game_projection = vmath.matrix4_orthographic(0, M.game_width , -M.height_delta, M.game_height - M.height_delta, -1, 1)
+	local aspect = screen_height/screen_width
+	local game_width=constants.game_zone_width
+	local game_height = constants.game_zone_height
+	local delta_x=0
+	local delta_y=0
+	
+	--biggest height. show more dirt and sky more than 4/3
+	if(aspect>=minimum_aspect)then
+		delta_y = ((game_width * aspect) - game_height)/2
 	else
-		--show game zone. But add offset in x.
-		local delta = (1.5 - height_to_width) * M.game_width
-		M.game_height = constants.game_zone_height
-		M.game_projection = vmath.matrix4_orthographic(-delta, M.game_width + delta,0, M.game_height, -1, 1)
+		--show minimum aspect in height. But add offset in x.
+		delta_x = (minimum_aspect - aspect) * game_width/2
+		delta_y = ((game_width * minimum_aspect) - game_height)/2
 	end	
+	M.max_height=game_height+delta_y
+	M.game_projection = vmath.matrix4_orthographic(-delta_x, constants.game_zone_width+delta_x , -delta_y, M.max_height, -1, 1)
 end	
 
 
